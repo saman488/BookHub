36658a051e0172a4e185517d2c365e9dac52c48b
saman <saman1817z@gmail.com>
2024-01-02 16:53:43 +0000

    add Carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
+    <div>
     <Navbar />
+    <ExploreTopBooks/>
+    <Carousel/>
+    </div>
 
   );
 }
