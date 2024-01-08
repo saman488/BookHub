e7e16df41afe468340b46b3457886c00985f9d72
saman <saman1817z@gmail.com>
2024-01-08 21:01:37 +0000

    set up new url aslo add new query and and call the api and check the response is ok convert the response

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -10,6 +10,39 @@ export const Carousel = () => {
 
     useEffect(() => {
         const fetchBooks = async () => {
+            const baseUrl: string = "http://localhost:8080/api/books";
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url);
+
+            if (response.ok) {
+                throw new Error('Something went wrong');
+
+            }
+
+            const responseJson = await response.json();
+
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailabale: responseData[key].copiesAvailabale,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
 
         };
         fetchBooks().catch((error: any) => {
