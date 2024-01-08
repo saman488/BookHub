f85d527d87f91d3ba03fcec8880818671067e097
saman <saman1817z@gmail.com>
2024-01-08 17:39:18 +0000

    restore deleted files

---
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -0,0 +1,88 @@
+import { ReturnBook } from "./ReturnBook";
+import { useEffect, useState } from "react";
+import BookModel from "../../../models/BookModel";
+import { error } from "console";
+
+export const Carousel = () => {
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httperror, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+
+    }, [])
+
+    return (
+        <div className='container mt-5' style={{ height: 550 }}>
+
+            <div className='homepage-carousel-title'>
+                <h3>Find your next "I stayed up too late reading" book.</h3>
+            </div>
+
+            <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 d-none d-lg-block' data-bs-interval='false'>
+
+                {/* Desktop */}
+                <div className='carousel-inner'>
+                    <div className='carousel-item active'>
+                        <div className='row d-felx justify-content-center align-items-center '>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
+
+                        </div>
+
+                    </div>
+
+                    <div className='carousel-item '>
+                        <div className='row d-felx justify-content-center align-item-center '>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
+
+                        </div>
+
+                    </div>
+
+                    <div className='carousel-item '>
+                        <div className='row d-felx justify-content-center align-item-center '>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
+                        </div>
+
+                    </div>
+                </div>
+
+                <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className='carousel-control-prev-icon' aria-hidden='true' ></span>
+                    <span className='visually-hidden'>Previous</span>
+                </button>
+                <button className='carousel-control-next' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                    <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Next</span>
+                </button>
+
+            </div>
+
+            {/* Mobile */}
+            <div className='d-lg-none mt-3'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    <ReturnBook />
+                </div>
+            </div>
+            <div className='HomePage-carousel-title mt-3'>
+                <a className='btn btn-outline-secondary btn-lg' href='#'>View More</a>
+            </div>
+        </div>
+
+
+    );
+
+}
---
+++ src/models/BookModel.ts
@@ -0,0 +1,28 @@
+class BookModel {
+    id: number;
+    title: string;
+    author?: string;
+    description?: string
+    copies?: number;
+    copiesAvailabale?: number;
+    category?: string;
+    img?: string;
+
+    constructor(id: number, title: string, author?: string,
+        description?: string,
+        copies?: number,
+        copiesAvailabale?: number,
+        category?: string,
+        img?: string) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.copies = copies;
+        this.copiesAvailabale = copiesAvailabale;
+        this.category = category;
+        this.img = img;
+    }
+}
+
+export default BookModel;   
